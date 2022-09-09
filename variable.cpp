de5a5984790c51e66fbc3a7f9c0335ba7498635c
Briana M. Nzivu <bnzivu@usiu.ac.ke>
2022-09-09 10:20:07 +0300

    Started on C++

---
+++ variable.cpp
@@ -0,0 +1,12 @@
+/// Program for addition oftwo  numbers
+//By 	Nzivu Briana
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, sum;
+	cout << "Enter two numbers:";
+	cin >> a >> b ;
+	sum = a + b;
+	cout<< a << " + " << b << " = " << sum;	
+}
